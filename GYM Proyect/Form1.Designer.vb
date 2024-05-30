b3fb8f80bd4ad41d9146d3837c9e000d7c97ca49
FALKON <campossoriao1@gmail.com>
2024-05-30 10:14:14 +0200

    Agregar archivos de proyecto.

---
+++ GYM Proyect/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLeerTXT = New System.Windows.Forms.Button()
+        Me.btnEscribirTXT = New System.Windows.Forms.Button()
+        Me.btnExportarDOCX = New System.Windows.Forms.Button()
+        Me.btnExportarXLSX = New System.Windows.Forms.Button()
+        Me.btnExportarXML = New System.Windows.Forms.Button()
+        Me.btnExportarJSON = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnLeerTXT
+        '
+        Me.btnLeerTXT.Location = New System.Drawing.Point(157, 55)
+        Me.btnLeerTXT.Name = "btnLeerTXT"
+        Me.btnLeerTXT.Size = New System.Drawing.Size(146, 29)
+        Me.btnLeerTXT.TabIndex = 0
+        Me.btnLeerTXT.Text = "btnLeerTXT"
+        Me.btnLeerTXT.UseVisualStyleBackColor = True
+        '
+        'btnEscribirTXT
+        '
+        Me.btnEscribirTXT.Location = New System.Drawing.Point(157, 20)
+        Me.btnEscribirTXT.Name = "btnEscribirTXT"
+        Me.btnEscribirTXT.Size = New System.Drawing.Size(146, 29)
+        Me.btnEscribirTXT.TabIndex = 1
+        Me.btnEscribirTXT.Text = "btnEscribirTXT"
+        Me.btnEscribirTXT.UseVisualStyleBackColor = True
+        '
+        'btnExportarDOCX
+        '
+        Me.btnExportarDOCX.Location = New System.Drawing.Point(157, 125)
+        Me.btnExportarDOCX.Name = "btnExportarDOCX"
+        Me.btnExportarDOCX.Size = New System.Drawing.Size(146, 29)
+        Me.btnExportarDOCX.TabIndex = 2
+        Me.btnExportarDOCX.Text = "btnExportarDOCX"
+        Me.btnExportarDOCX.UseVisualStyleBackColor = True
+        '
+        'btnExportarXLSX
+        '
+        Me.btnExportarXLSX.Location = New System.Drawing.Point(157, 160)
+        Me.btnExportarXLSX.Name = "btnExportarXLSX"
+        Me.btnExportarXLSX.Size = New System.Drawing.Size(146, 29)
+        Me.btnExportarXLSX.TabIndex = 3
+        Me.btnExportarXLSX.Text = "btnExportarXLSX"
+        Me.btnExportarXLSX.UseVisualStyleBackColor = True
+        '
+        'btnExportarXML
+        '
+        Me.btnExportarXML.Location = New System.Drawing.Point(157, 195)
+        Me.btnExportarXML.Name = "btnExportarXML"
+        Me.btnExportarXML.Size = New System.Drawing.Size(146, 29)
+        Me.btnExportarXML.TabIndex = 4
+        Me.btnExportarXML.Text = "btnExportarXML"
+        Me.btnExportarXML.UseVisualStyleBackColor = True
+        '
+        'btnExportarJSON
+        '
+        Me.btnExportarJSON.Location = New System.Drawing.Point(157, 90)
+        Me.btnExportarJSON.Name = "btnExportarJSON"
+        Me.btnExportarJSON.Size = New System.Drawing.Size(146, 29)
+        Me.btnExportarJSON.TabIndex = 5
+        Me.btnExportarJSON.Text = "btnExportarJSON"
+        Me.btnExportarJSON.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(343, 31)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(440, 205)
+        Me.DataGridView1.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnExportarJSON)
+        Me.Controls.Add(Me.btnExportarXML)
+        Me.Controls.Add(Me.btnExportarXLSX)
+        Me.Controls.Add(Me.btnExportarDOCX)
+        Me.Controls.Add(Me.btnEscribirTXT)
+        Me.Controls.Add(Me.btnLeerTXT)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnLeerTXT As Button
+    Friend WithEvents btnEscribirTXT As Button
+    Friend WithEvents btnExportarDOCX As Button
+    Friend WithEvents btnExportarXLSX As Button
+    Friend WithEvents btnExportarXML As Button
+    Friend WithEvents btnExportarJSON As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ GYM Proyect/Form1.vb
@@ -0,0 +1,232 @@
+﻿Imports System
+Imports System.IO
+Imports System.Xml.Serialization
+Imports Newtonsoft.Json
+Imports OfficeOpenXml
+Imports Xceed.Words.NET
+
+Public Class Form1
+    ' Propiedades privadas
+    Private _nombre As String
+    Private _edad As Integer
+    Private _direccion As String
+
+    ' Propiedad solo lectora
+    Public ReadOnly Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+    End Property
+
+    ' Propiedad solo escritura
+    Public WriteOnly Property Edad As Integer
+        Set(value As Integer)
+            _edad = value
+        End Set
+    End Property
+
+    ' Propiedad de lectura y escritura
+    Public Property Direccion As String
+        Get
+            Return _direccion
+        End Get
+        Set(value As String)
+            _direccion = value
+        End Set
+    End Property
+
+    ' Constructor sin parámetros
+    Public Sub New()
+        InitializeComponent()
+
+        ' Inicializar propiedades
+        _nombre = "Desconocido"
+        _edad = 0
+        _direccion = "Desconocida"
+
+        ' Inicializar componentes
+        InicializarComponentes()
+    End Sub
+
+    ' Constructor con parámetros
+    Public Sub New(nombre As String, edad As Integer, direccion As String)
+        InitializeComponent()
+
+        ' Inicializar propiedades
+        _nombre = nombre
+        _edad = edad
+        _direccion = direccion
+
+        ' Inicializar componentes
+        InicializarComponentes()
+    End Sub
+
+    ' Método estático
+    Public Shared Sub Saludar()
+        Console.WriteLine("Hola!")
+    End Sub
+
+    ' Método que no recibe parámetros ni regresa nada
+    Public Sub MostrarInformacion()
+        Console.WriteLine($"Nombre: {_nombre}, Edad: {_edad}, Dirección: {_direccion}")
+    End Sub
+
+    ' Método que recibe parámetros pero no regresa nada
+    Public Sub ActualizarEdad(nuevaEdad As Integer)
+        _edad = nuevaEdad
+    End Sub
+
+    ' Método que no recibe parámetros pero regresa algo
+    Public Function ObtenerNombre() As String
+        Return _nombre
+    End Function
+
+    ' Método que recibe parámetros y regresa algo
+    Public Function ObtenerDetalle(completo As Boolean) As String
+        If completo Then
+            Return $"Nombre: {_nombre}, Edad: {_edad}, Dirección: {_direccion}"
+        Else
+            Return _nombre
+        End If
+    End Function
+
+    ' Control de excepciones en tiempo de ejecución
+    Public Sub LeerArchivo(path As String)
+        Try
+            Dim contenido As String = File.ReadAllText(path)
+            Console.WriteLine(contenido)
+        Catch ex As Exception
+            Console.WriteLine($"Error leyendo el archivo: {ex.Message}")
+        End Try
+    End Sub
+
+    ' Archivos usando diálogos - Lectura de un TXT
+    Public Sub LeerTXT()
+        Dim openFileDialog As New OpenFileDialog
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Try
+                Dim contenido As String = File.ReadAllText(openFileDialog.FileName)
+                Console.WriteLine(contenido)
+            Catch ex As Exception
+                Console.WriteLine($"Error leyendo el archivo: {ex.Message}")
+            End Try
+        End If
+    End Sub
+
+    ' Archivos usando diálogos - Escritura de un TXT
+    Public Sub EscribirTXT()
+        Dim saveFileDialog As New SaveFileDialog
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Try
+                File.WriteAllText(saveFileDialog.FileName, "Contenido de ejemplo")
+                Console.WriteLine("Archivo guardado exitosamente.")
+            Catch ex As Exception
+                Console.WriteLine($"Error escribiendo el archivo: {ex.Message}")
+            End Try
+        End If
+    End Sub
+
+    ' Exportar DOCX
+    Public Sub ExportarDOCX()
+        Dim saveFileDialog As New SaveFileDialog
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim doc As DocX = DocX.Create(saveFileDialog.FileName)
+            doc.InsertParagraph("Contenido de ejemplo para DOCX.")
+            doc.Save()
+            Console.WriteLine("Archivo DOCX guardado exitosamente.")
+        End If
+    End Sub
+
+    ' Exportar XLSX
+    Public Sub ExportarXLSX()
+        Dim saveFileDialog As New SaveFileDialog
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Using package As New ExcelPackage(New FileInfo(saveFileDialog.FileName))
+                Dim worksheet As ExcelWorksheet = package.Workbook.Worksheets.Add("Hoja1")
+                worksheet.Cells(1, 1).Value = "Contenido de ejemplo para XLSX."
+                package.Save()
+                Console.WriteLine("Archivo XLSX guardado exitosamente.")
+            End Using
+        End If
+    End Sub
+
+    ' Exportar XML
+    Public Sub ExportarXML()
+        Dim saveFileDialog As New SaveFileDialog
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim xmlSerializer As New XmlSerializer(GetType(Form1))
+            Using stream As New FileStream(saveFileDialog.FileName, FileMode.Create)
+                xmlSerializer.Serialize(stream, Me)
+                Console.WriteLine("Archivo XML guardado exitosamente.")
+            End Using
+        End If
+    End Sub
+
+    ' Exportar JSON
+    Public Sub ExportarJSON()
+        Dim saveFileDialog As New SaveFileDialog
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim json As String = JsonConvert.SerializeObject(Me)
+            File.WriteAllText(saveFileDialog.FileName, json)
+            Console.WriteLine("Archivo JSON guardado exitosamente.")
+        End If
+    End Sub
+
+    Private Sub btnLeerTXT_Click(sender As Object, e As EventArgs) Handles btnLeerTXT.Click
+        LeerTXT()
+    End Sub
+    Private Sub btnEscribirTXT_Click_1(sender As Object, e As EventArgs) Handles btnEscribirTXT.Click
+        EscribirTXT()
+    End Sub
+
+    Private Sub btnExportarDOCX_Click(sender As Object, e As EventArgs) Handles btnExportarDOCX.Click
+        ExportarDOCX()
+    End Sub
+
+    Private Sub btnExportarXLSX_Click(sender As Object, e As EventArgs) Handles btnExportarXLSX.Click
+        ExportarXLSX()
+    End Sub
+
+    Private Sub btnExportarXML_Click(sender As Object, e As EventArgs) Handles btnExportarXML.Click
+        ExportarXML()
+    End Sub
+
+    Private Sub btnExportarJSON_Click(sender As Object, e As EventArgs) Handles btnExportarJSON.Click
+        ExportarJSON()
+    End Sub
+
+    ' Método para inicializar los componentes del formulario
+    Private Sub InicializarComponentes()
+        Me.btnLeerTXT = New Button() With {.Text = "Leer TXT"}
+        Me.btnEscribirTXT = New Button() With {.Text = "Escribir TXT"}
+        Me.btnExportarDOCX = New Button() With {.Text = "Exportar DOCX"}
+        Me.btnExportarXLSX = New Button() With {.Text = "Exportar XLSX"}
+        Me.btnExportarXML = New Button() With {.Text = "Exportar XML"}
+        Me.btnExportarJSON = New Button() With {.Text = "Exportar JSON"}
+
+        ' Agregar los botones al formulario
+        Me.Controls.Add(btnLeerTXT)
+        Me.Controls.Add(btnEscribirTXT)
+        Me.Controls.Add(btnExportarDOCX)
+        Me.Controls.Add(btnExportarXLSX)
+        Me.Controls.Add(btnExportarXML)
+        Me.Controls.Add(btnExportarJSON)
+
+
+
+        ' Posicionar los botones en el formulario
+        Dim buttons = New Button() {btnLeerTXT, btnEscribirTXT, btnExportarDOCX, btnExportarXLSX, btnExportarXML, btnExportarJSON}
+        Dim yPosition As Integer = 20
+        For Each btn As Button In buttons
+            btn.Top = yPosition
+            btn.Left = 20
+            yPosition += 40
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnLeerTXT.Click
+
+    End Sub
+
+
+End Class
